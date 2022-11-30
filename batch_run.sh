32c1f98c347ad567d4ae192e03a308765339f11c
literaryno4 <m18260037123@163.com>
2022-11-30 16:32:51 +0800

    update run script

--- batch_run.sh
+++ batch_run.sh
@@ -7,7 +7,7 @@ for dataset in ${datasets}; do
     for target_model in ${models}; do
         for shadow_model in ${models}; do
             cur_time=$(date +%y%m%d-%H:%M:%S)
-            logfile="${dataset}-${target_model}-${shadow_model}-${attack_mode}-${cur_time}.log"
+            logfile="$(pwd)/log/${dataset}-${target_model}-${shadow_model}-${attack_mode}-${cur_time}.log"
             echo ${logfile}
             echo ${dataset} ${target_model} ${shadow_model} ${attack_mode} ${cur_time};
             python main.py \
